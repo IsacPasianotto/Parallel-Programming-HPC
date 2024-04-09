b5a945106e64a1e43835d6fd3dae9722fb676316
pasianeight <yh4mop12z@gmail.com>
2024-04-09 18:00:35 +0200

    refactor

--- mat-mat-mul/src/init.c
+++ mat-mat-mul/src/init.c
@@ -11,15 +11,14 @@ void init_local_matrix (double* M, long int n_elements)
   #pragma omp parallel for
   for (long int i = 0; i < n_elements; i++)
   {
-    // M[i] = (double) rand()/1000000;
-    M[i] = (double) i;
+    M[i] = (double) rand()/1000000;
 #ifdef SMALL_INTS
     M[i] = (double) i;
 #endif
     // 50% of chance to be negative -> help to avoid overflow
-//    if (rand() % 2 == 0)
-//    {
-//      M[i] *= -1;
-//    }
+    if (rand() % 2 == 0)
+    {
+      M[i] *= -1;
+    }
   }
 }
--- mat-mat-mul/src/product.c
+++ mat-mat-mul/src/product.c
@@ -5,25 +5,38 @@
 
 #include "product.h"
 
-void compute_block_result_naive(double* local_C_block, double* A, double* buffer, long int N, long int local_size, int* all_sizes, int iter) {
+void compute_block_result_naive(double* local_C_block, double* A, double* buffer, long int N, long int local_size, int* all_sizes, int iter)
+{
   // A: local_size x N
   // buffer: N x all_sizes[iter]
   // local_C_block: local_size x all_sizes[iter]
-  for (int i = 0; i < local_size; i++) {
-    for (int j = 0; j < all_sizes[iter]; j++) {
+
+  #pragma omp parallel for
+  for (int i = 0; i < local_size; i++)
+  {
+    #pragma omp simd
+    for (int j = 0; j < all_sizes[iter]; j++)
+    {
       double sum = 0.0;
-      for (int k = 0; k < N; k++) {
+      #pragma omp simd
+      for (int k = 0; k < N; k++)
+      {
         sum += A[i * N + k] * buffer[k * all_sizes[iter] + j];
       }
       local_C_block[i * all_sizes[iter] + j] = sum;
-    }
-  }
+    } // loop over j
+  } // loop over i
 }
 
-void copy_block_to_global_C(double* C, double* local_C_block, long int N, long int local_size, int* all_sizes, int size, int iter) {
+void copy_block_to_global_C(double* C, double* local_C_block, long int N, long int local_size, int* all_sizes, int size, int iter)
+{
   long int index = iter * ((N % size) > 0 ? N / size + 1 : N / size);
-  for (int i = 0; i < local_size; i++) {
-    for (int j = 0; j < all_sizes[iter]; j++) {
+  #pragma omp parallel for
+  for (int i = 0; i < local_size; i++)
+  {
+    #pragma omp simd
+    for (int j = 0; j < all_sizes[iter]; j++)
+    {
       C[index + i * N + j] = local_C_block[i * all_sizes[iter] + j];
     }
   }
