164bfabe1db97af945f453bca7bd5f8c7a14b483
Isac Pasianotto <ipasiano@login07.leonardo.local>
2024-04-10 22:41:01 +0200

    first measurements

--- mat-mat-mul/main.c
+++ mat-mat-mul/main.c
@@ -31,8 +31,6 @@ int main(int argc, char* argv[])
   int rank, size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
-  
-  omp_set_nested(1);
 
   /*--------------------------------------------------*
    | 1. Compute the local quantities for each worker  |
--- mat-mat-mul/sbatcher.sh
+++ mat-mat-mul/sbatcher.sh
@@ -3,15 +3,15 @@
 #SBATCH --job-name="mmm-pt1"
 #SBATCH --get-user-env
 #SBATCH --partition=dcgp_usr_prod
-#SBATCH --nodes=1
-#SBATCH --ntasks=1                   # Every MPI proc is a task --> = n nodes
+#SBATCH --nodes=16
+#SBATCH --ntasks=16                   # Every MPI proc is a task --> = n nodes
 #SBATCH --ntasks-per-node=1          # Number of tasks (or processes) per node
 #SBATCH --cpus-per-task=56           # Number of CPU cores per task
-#SBATCH --mem=256G
+#SBATCH --mem=15G
 #SBATCH --time=02:00:00
 
-nproc=2      #number of MPI-processes
-matsize=2000
+nproc=16      #number of MPI-processes
+matsize=5000
 
 # Standard preamble
 echo "---------------------------------------------"
@@ -25,8 +25,8 @@ echo "---------------------------------------------"
 # TODO: change the module load commands
 #       according to the architecture you are using
 module purge
-module load openMPI/4.1.5/gnu/
-
+#  module load openMPI/4.1.5/gnu/
+module load openmpi/4.1.6--gcc--12.2.0
 
 # Remove old files if any exist and then compile
 make clean
@@ -40,10 +40,10 @@ export OMP_PLACES=cores
 export OMP_PROC_BIND=close
 
 # if OMP_NUM_THREADS is not set, the number of threads is equal to the number of cores
-# export OMP_NUM_THREADS=128
+# export OMP_NUM_THREADS=56
 
 # Comment after first time:
-echo "time(s),rank-worker,number-of-nodes,algorithm,thing-measured" > data.csv
+# echo "time(s),rank-worker,number-of-nodes,algorithm,thing-measured" > data.csv
 
 mpirun -np $nproc ./main.x $matsize >> data.csv
 
