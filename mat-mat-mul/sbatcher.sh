e404cf471cd60245649148a67c655e576f10b79e
IsacPasianotto <yh4mop12z@gmail.com>
2024-04-10 23:16:35 +0200

    other measuraments

--- mat-mat-mul/sbatcher.sh
+++ mat-mat-mul/sbatcher.sh
@@ -3,14 +3,14 @@
 #SBATCH --job-name="mmm-pt1"
 #SBATCH --get-user-env
 #SBATCH --partition=dcgp_usr_prod
-#SBATCH --nodes=16
-#SBATCH --ntasks=16                   # Every MPI proc is a task --> = n nodes
+#SBATCH --nodes=32
+#SBATCH --ntasks=32                   # Every MPI proc is a task --> = n nodes
 #SBATCH --ntasks-per-node=1          # Number of tasks (or processes) per node
 #SBATCH --cpus-per-task=56           # Number of CPU cores per task
 #SBATCH --mem=15G
 #SBATCH --time=02:00:00
 
-nproc=16      #number of MPI-processes
+nproc=32      #number of MPI-processes
 matsize=5000
 
 # Standard preamble
@@ -27,6 +27,7 @@ echo "---------------------------------------------"
 module purge
 #  module load openMPI/4.1.5/gnu/
 module load openmpi/4.1.6--gcc--12.2.0
+module load openblas/0.3.24--gcc--12.2.0
 
 # Remove old files if any exist and then compile
 make clean
