fe43714e2e5bd4e3178b6ec0611083328e470fef
pasianeight <yh4mop12z@gmail.com>
2024-04-13 10:00:51 +0200

    forgotten headers, now it should compile

--- mat-mat-mul/include/product.h
+++ mat-mat-mul/include/product.h
@@ -7,3 +7,9 @@ void compute_block_result_naive (double* local_C_block, double* A, double* buffe
 #define COPY_BLOCK_TO_GLOBAL_C
 void copy_block_to_global_C (double* C, double* local_C_block, long int N, long int local_size, int* all_sizes,  int size, int iter);
 #endif
+
+#ifdef CUDA
+#ifndef CUDA_COPY_BLOCK_TO_GLOBAL_C
+#define CUDA_COPY_BLOCK_TO_GLOBAL_C
+__global__ void cuda_copy_block_to_global_c (double* d_C, double* local_C_block, long int N, long int local_size, int* all_sizes, int size, int iter);
+#endif
--- mat-mat-mul/include/stopwatch.h
+++ mat-mat-mul/include/stopwatch.h
@@ -12,3 +12,10 @@
 #define PRINT_TIME_RECORDS
   void print_time_records(double* time_records, int rank, int size, const char* program_type);
 #endif
+
+#ifdef CUDA
+#ifndef PRINT_TIME_RECORDS_CUDA
+#define PRINT_TIME_RECORDS_CUDA
+  void print_time_records_cuda(double* time_records, int rank, int size, const char* program_type);
+#endif
+#endif
