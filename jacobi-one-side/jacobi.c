2745c9849753f5541c896165feda23720de0556d
pasianeight <yh4mop12z@gmail.com>
2024-06-20 21:04:17 +0200

    checkpoint-backup

--- jacobi-one-side/jacobi.c
+++ jacobi-one-side/jacobi.c
@@ -2,14 +2,13 @@
  | file: jacobi.c                              |
  | author: Ivan Girotto  (Prof. of the course) |
  | edited by: Isac Pasianotto                  |
- | date: 2024-05                               |
+ | date: 2024-04                               |
  | context: exam of "Parallel programming for  |
  |      HPC". Msc Course in DSSC               |
  | description: Jacobi method for solving a    |
- |      Laplace equation, using the one-side   |
- |      communication MPI paradigm.            |
+ |      Laplace equation, ported on GPU using  |
+ |      openacc and distributed using MPI      |
  *---------------------------------------------*/
-
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -19,63 +18,52 @@
 #include <omp.h>
 
 /*** function declarations ***/
-
-// save matrix to .dat file in order to render with gnuplot
-void save_gnuplot(double *M, size_t dim);
 // return the elapsed time
 double seconds(void);
 // mpi-needs functions
 int calculate_local_size(int tot_col, int size, int rank);
-void set_recvcout(int *recvcount, int size, int N);
-void set_displacement(int *displacement, const int *recvcount, int size);
+void set_recvcout(int* recvcount, int size, int N);
+void set_displacement(int* displacement, const int* recvcount, int size);
 
 /*** end function declaration ***/
 
-/*** main ***/
+/***    main     ***/
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-  /** MPI initialization **/
+
+  /**   MPI initialization   **/
   int provided;
   MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
   int rank, size;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
-  /** end MPI initialization **/
 
-  /** Variables initialization **/
+  /**   end MPI initialization   **/
+
+
+  /**   Variables initialization   **/
+
   // -- declaration
-  double increment; // timing variables
-  double communication_time = 0, compute_time = 0;
-  double start_time = 0, end_time;
+  double increment;
+  #ifdef STOPWATCH
+    double communication_time=0, compute_time=0;  // timing variables
+    double start_init, end_init;
+  #endif
 
-  size_t i, j, it;                  // indexes for loops
-  double *matrix, *matrix_new, *tmp_matrix; // initialize matrix
+  size_t i, j, it;                             // indexes for loops
+  double *matrix, *matrix_new, *tmp_matrix;    // initialize matrix
   size_t dimension = 0, iterations = 0;
   size_t byte_dimension = 0;
-  // send up, recv bottom
-  int send_to;
-  int recv_from;
-#ifdef ONESIDE
-  MPI_Win win;
-#endif
-
 
   if (argc != 3)
   {
     fprintf(stderr, "\nwrong number of arguments. Usage: ./a.out dim it\n");
     return 1;
   }
+
   dimension = atoi(argv[1]);
   iterations = atoi(argv[2]);
-
-#ifdef VERBOSE
-  if (rank == 0)
-      {
-          printf("matrix size = %zu\n", dimension);
-          printf("number of iterations = %zu\n", iterations);
-      }
-#endif
 
   size_t local_size = dimension / size;
   if (rank < (dimension % size))
@@ -85,17 +73,19 @@ int main(int argc, char *argv[])
 
   // -- allocation
   byte_dimension = sizeof(double) * (local_size + 2) * (dimension + 2);
-  matrix = (double *)malloc(byte_dimension);
-  matrix_new = (double *)malloc(byte_dimension);
+  matrix = (double*)malloc(byte_dimension);
+  matrix_new = (double*)malloc(byte_dimension);
 
   memset(matrix, 0, byte_dimension);
   memset(matrix_new, 0, byte_dimension);
 
   // -- initialization
-  start_time = seconds();
+  #ifdef STOPWATCH
+    start_init = seconds();
+  #endif
 
   // fill initial values
-#pragma omp parallel for collapse(2)
+  #pragma omp parallel for collapse(2)
   for (i = 1; i <= local_size; ++i)
   {
     for (j = 1; j <= dimension; ++j)
@@ -108,7 +98,7 @@ int main(int argc, char *argv[])
   // Compute the offset
   int size_std = (dimension + 2) / size;
   int size_reminder = size_std + 1;
-  int diff = (dimension + 2) % size; // The number of blocks that have n_reminder elements
+  int diff = (dimension + 2) % size;  // The number of blocks that have n_reminder elements
   int offset = (rank < diff) ? rank * size_reminder : diff * size_std + (rank - diff) * size_std;
 
   // need to initialize only the vertical borders
@@ -127,182 +117,159 @@ int main(int argc, char *argv[])
       matrix_new[((local_size + 1) * (dimension + 2)) + (dimension + 1 - i)] = i * increment;
     }
   }
-  end_time = seconds();
-
-  // compute upper and lower bounds for the communication
-  send_to= (rank - 1) >= 0 ? rank - 1 : MPI_PROC_NULL;
-  recv_from = (rank + 1) < size ? rank + 1 : MPI_PROC_NULL;
-
+  #ifdef STOPWATCH
+    end_init = seconds();
+  #endif
   /** end of variable initialization **/
 
-  /** start the actual algorithm **/
+  /** start the actual algorithm  **/
   for (it = 0; it < iterations; ++it)
   {
-    /*   communication of the borders   */
-    double comm_time_start = seconds();
+    // send up, recv bottom
+    int proc_above = (rank - 1) >= 0 ? rank - 1 : MPI_PROC_NULL;
+    int proc_below = (rank + 1) < size ? rank + 1 : MPI_PROC_NULL;
+    double time = seconds();
 
-#ifndef ONESIDE
-    MPI_Sendrecv(matrix + (dimension + 2), dimension + 2, MPI_DOUBLE, send_to, 0,
-                 matrix + (dimension + 2) * (local_size + 1), dimension + 2, MPI_DOUBLE, recv_from, 0,
-                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    MPI_Sendrecv(matrix + (dimension + 2) * local_size, dimension + 2, MPI_DOUBLE, recv_from, 0,
-                 matrix, dimension + 2, MPI_DOUBLE, send_to, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-#else
-    /*
-       * One sided communication: each process opens a memory window to the other processes
-       * Only to exercise the one-sided communication:
-       *  - PUT the data in the window
-       *  - GET the data from the window
-       */
-
-      // Define a window for the matrix
+  #ifndef ONESIDE
+    MPI_Sendrecv(matrix + (dimension + 2), dimension + 2, MPI_DOUBLE, proc_above, 0, matrix + (dimension + 2) * (local_size + 1), dimension + 2, MPI_DOUBLE, proc_below, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Sendrecv(matrix + (dimension + 2) * local_size, dimension + 2, MPI_DOUBLE, proc_below, 2, matrix, dimension + 2, MPI_DOUBLE, proc_above, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  #else
+    //-- Two-sided communication
+    #ifdef ONEWIN    // open one window (try to reduce overhead)
+      MPI_Win win;
       MPI_Win_create(matrix, byte_dimension, sizeof(double), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
-
       #ifdef PUT
-        // Put upper border to the previous process
         MPI_Win_fence(0, win);
-        if (send_to != MPI_PROC_NULL) {
-          MPI_Put(matrix + (dimension + 2), dimension + 2, MPI_DOUBLE, send_to, (dimension + 2) * (local_size + 1), dimension + 2, MPI_DOUBLE, win);
-        }
+        if (proc_above != MPI_PROC_NULL)
+          MPI_Put(matrix + (dimension + 2), dimension + 2, MPI_DOUBLE, proc_above, (dimension + 2) * local_size, dimension + 2, MPI_DOUBLE, win);
+        if (proc_below != MPI_PROC_NULL)
+          MPI_Put(matrix + (dimension + 2) * local_size, dimension + 2, MPI_DOUBLE, proc_below, 0, dimension + 2, MPI_DOUBLE, win);
         MPI_Win_fence(0, win);
-
-        // Put lower border to the next process
+      #else // end of ifdef PUT --> if GET
         MPI_Win_fence(0, win);
-        if (recv_from != MPI_PROC_NULL) {
-          MPI_Put(matrix + (dimension + 2) * local_size, dimension + 2, MPI_DOUBLE, recv_from, 0, dimension + 2, MPI_DOUBLE, win);
-        }
-        MPI_Win_fence(0, win);
-
-      #else // ifdef GET
-        // Get upper border from the previous process
-        MPI_Win_fence(0, win);
-        if (send_to != MPI_PROC_NULL) {
-          MPI_Get(matrix + (dimension + 2) * (local_size + 1), dimension + 2, MPI_DOUBLE, send_to, (dimension + 2) * (local_size + 1), dimension + 2, MPI_DOUBLE, win);
-        }
-        MPI_Win_fence(0, win);
-
-        // Get lower border from the next process
-        MPI_Win_fence(0, win);
-        if (recv_from != MPI_PROC_NULL) {
-          MPI_Get(matrix, dimension + 2, MPI_DOUBLE, recv_from, 0, dimension + 2, MPI_DOUBLE, win);
-        }
+        if (proc_above != MPI_PROC_NULL)
+          MPI_Get(matrix, dimension + 2, MPI_DOUBLE, proc_above, (dimension + 2) * local_size, dimension + 2, MPI_DOUBLE, win);
+        if (proc_below != MPI_PROC_NULL)
+          MPI_Get(matrix + (dimension + 2) * (local_size + 1), dimension + 2, MPI_DOUBLE, proc_below, 0, dimension + 2, MPI_DOUBLE, win);
         MPI_Win_fence(0, win);
       #endif  // end of ifdef GET
-
       MPI_Win_free(&win);
-#endif  // end of  ifdef ONESIDE
+    #else   // end of one window condition
+      // open two windows
+      double* ghost_up = matrix;
+      double* ghost_down = matrix + (local_size + 1) * (dimension + 2);
 
-    communication_time += seconds() - comm_time_start;
-    /*   end of communication of the borders   */
+      double* first_row_point = matrix + (dimension + 2);
+      double* last_row_point = matrix + (dimension + 2) * local_size;
 
+      MPI_Win ghost_up_win, ghost_down_win;
+      MPI_Win_create(ghost_up, (MPI_Aint)dimension * sizeof(double), sizeof(double), MPI_INFO_NULL, MPI_COMM_WORLD, &ghost_up_win);
+      MPI_Win_create(ghost_down, (MPI_Aint)dimension * sizeof(double), sizeof(double), MPI_INFO_NULL, MPI_COMM_WORLD, &ghost_down_win);
 
-    double compute_time_start = seconds();
+      #ifdef PUT
+      MPI_Win_fence(0, ghost_up_win);
+      MPI_Win_fence(0, ghost_down_win);
+      if (proc_above != MPI_PROC_NULL)
+        MPI_Put(first_row_point, dimension, MPI_DOUBLE, proc_above, 0, dimension, MPI_DOUBLE, ghost_down_win);
+      if (proc_below != MPI_PROC_NULL)
+        MPI_Put(last_row_point, dimension, MPI_DOUBLE, proc_below, 0, dimension, MPI_DOUBLE, ghost_up_win);
+      MPI_Win_fence(0, ghost_down_win);
+      MPI_Win_fence(0, ghost_up_win);
+      MPI_Win_free(&ghost_up_win);
+      MPI_Win_free(&ghost_down_win);
+      #else
+        printf("GET NOT IMPLEMENTED YET\n");
+      #endif  // end of ifdef PUT
+    #endif  // end of two window condition
+  #endif  // end of ONESIDE condition
+
+    #ifdef STOPWATCH
+      communication_time += seconds() - time;
+      time = seconds();
+    #endif
 
     /** evolve the matrix **/
-#pragma omp parallel for collapse(2)
-    for (i = 1; i <= local_size; ++i)
+
+    #pragma omp parallel for collapse(2)
+    for (size_t i = 1; i <= local_size; ++i)
     {
-      for (j = 1; j <= dimension; ++j)
+      for (size_t j = 1; j <= dimension; ++j)
       {
-        matrix_new[(i * (dimension + 2)) + j] = 0.25 * (
-            matrix[((i - 1) * (dimension + 2)) + j] +
-            matrix[(i * (dimension + 2)) + (j + 1)] +
-            matrix[((i + 1) * (dimension + 2)) + j] +
-            matrix[(i * (dimension + 2)) + (j - 1)]);
+        matrix_new[(i * (dimension + 2)) + j] = 0.25 * (matrix[((i - 1) * (dimension + 2)) + j] + matrix[(i * (dimension + 2)) + (j + 1)] + matrix[((i + 1) * (dimension + 2)) + j] + matrix[(i * (dimension + 2)) + (j - 1)]);
       }
     }
+    #ifdef STOPWATCH
+      compute_time += seconds() - time;
+    #endif
 
-    compute_time += seconds() - compute_time_start;
-
-    // swap pointers
+    // swap the pointers
     tmp_matrix = matrix;
     matrix = matrix_new;
     matrix_new = tmp_matrix;
   } /* end of loop over iteration iterations */
-
-  /** end of the matrix evolution **/
-
-  /** Save the results **/
   MPI_Barrier(MPI_COMM_WORLD);
+  /**   end of the matrix evolution   **/
 
-  // use MPI-IO to write the file in parallel.
-  MPI_File fh;
-  MPI_Offset file_disp = 0;
-  MPI_File_open(MPI_COMM_WORLD, "solution.dat", MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
-  // set the offset
-  for (int i = 0; i < rank; ++i)
-  {
-    file_disp += calculate_local_size(dimension, size, i) * (dimension + 2) * sizeof(double);
-  }
-  MPI_File_write_at(fh, file_disp, matrix, (dimension + 2) * (local_size + 2), MPI_DOUBLE, MPI_STATUS_IGNORE);
-  MPI_File_close(&fh);
+  /**   Save the results   **/
+    MPI_File fh;
+    MPI_Offset file_disp = 0;
+    MPI_File_open(MPI_COMM_WORLD, "solution.dat", MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &fh);
+    // set the offset
+    for( int i=0; i<rank; ++i)
+    {
+      file_disp+=calculate_local_size(dimension,size,i)*(dimension+2)*sizeof(double);
+    }
+    MPI_File_write_at(fh, file_disp, matrix, (dimension+2)*(local_size+2), MPI_DOUBLE, MPI_STATUS_IGNORE);
+    MPI_File_close(&fh);
   /** end of saving the results **/
 
-  /** Print the times **/
-#ifdef STOPWATCH
-  // time, rank, size, what
-    printf("%.10f,%d,%d,%s\n", end_time - start_time, rank, size, "matrix-initialization");
-    #ifdef _OPENACC
-    printf("%.10f,%d,%d,%s\n", copyin_end - copyin_start, rank, size, "copy-matrix-cpu-to-gpu");
-    printf("%.10f,%d,%d,%s\n", copyout_end - copyout_start, rank, size, "copy-matrix-gpu-to-cpu");
-    #endif
+  /**  Print the times  **/
+  #ifdef STOPWATCH
+    // time, rank, size, what
+    printf("%.10f,%d,%d,%s\n", end_init - start_init, rank, size, "matrix-initialization");
     printf("%.10f,%d,%d,%s\n", communication_time, rank, size, "mpi-send-rec");
     printf("%.10f,%d,%d,%s\n", compute_time, rank, size, "computation");
-#endif
-  /** End of printing time **/
+  #endif
+  /**  End of printing time **/
 
-  /** Finalize the program **/
-  free(matrix);
-  free(matrix_new);
+  /**   Finalize the program   **/
+
+  free( matrix );
+  free( matrix_new );
 
   MPI_Finalize();
   return 0;
 }
 
-/*** end of main ***/
+/***  end of main ***/
+
 
 /*** function definitions ***/
-
-void save_gnuplot(double *M, size_t dimension)
-{
-  size_t i, j;
-  const double h = 0.1;
-  FILE *file = fopen("solution.dat", "w");
-  for (i = 0; i <= dimension + 1; ++i)
-  {
-    for (j = 0; j <= dimension + 1; ++j)
-    {
-      fprintf(file, "%f\t%f\t%f\n", h * j, -h * i, M[(i * (dimension + 2)) + j]);
-    }
-  }
-  fclose(file);
-}
-
-double seconds()
+double seconds()        // A Simple timer for measuring the walltime
 {
   struct timeval tmp;
   double sec;
-  gettimeofday(&tmp, (struct timezone *)0);
-  sec = tmp.tv_sec + ((double)tmp.tv_usec) / 1000000.0;
+  gettimeofday( &tmp, (struct timezone *)0 );
+  sec = tmp.tv_sec + ((double)tmp.tv_usec)/1000000.0;
   return sec;
 }
 
-int calculate_local_size(int tot_col, int size, int rank)
+int calculate_local_size(int tot_col,int size,int rank)   //calculate how many row belong to the current rank
 {
-  return (rank < tot_col % size) ? tot_col / size + 1 : tot_col / size;
+  return (rank < tot_col % size) ? tot_col/size +1 : tot_col/size;
 }
 
-void set_recvcout(int *recvcount, int size, int N)
+void set_recvcout(int* recvcount, int size,int N)       //set the recv_count array
 {
-  for (int p = 0; p < size; ++p)
+  for(int p=0;p<size;++p)
   {
-    recvcount[p] = calculate_local_size(N, size, p) * (N + 2);
+    recvcount[p]=calculate_local_size(N,size,p)*(N+2);
   }
-  recvcount[0] += N + 2;
-  recvcount[size - 1] += N + 2;
+  recvcount[0]+=N+2;
+  recvcount[size-1]+=N+2;
 }
 
-void set_displacement(int *displacement, const int *recvcount, int size)
+void set_displacement(int* displacement,const int* recvcount,int size)    //calculate the displacement array using the recv_count array
 {
   displacement[0] = 0;
   for (int p = 1; p < size; ++p)
@@ -310,5 +277,4 @@ void set_displacement(int *displacement, const int *recvcount, int size)
     displacement[p] = displacement[p - 1] + recvcount[p - 1];
   }
 }
-/*** end of function definitions ***/
-
+/***  end of function declaration   ***/
